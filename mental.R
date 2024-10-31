873a20ee593cc67e0514094bb5d0ba961ef84070
Man Njoro <elijohnmwoho@gmail.com>
2024-10-31 23:11:42 +0300

    first commit

---
+++ mental.R
@@ -0,0 +1,30 @@
+# getwd()
+# setwd("D:/R-projo")
+df <- read.csv("Student Mental health.csv")
+df
+dim(df)
+str(df)
+summary(df)
+attach(df)
+hist(Age)
+class(Your.current.year.of.Study)
+unique(What.is.your.course.)
+df <- df[, 2:11]
+View(sort(table(What.is.your.course.), decreasing = TRUE))
+barplot(sort(table(What.is.your.course.), decreasing = TRUE))
+View(df[is.na(Age),])
+# Install necessary packages
+# install.packages("tidyverse")
+library("tidyverse")
+# Handle NA values in Age column
+# Get the median of Age
+ageMid = median(Age, na.rm=TRUE)
+ageMid
+df <- df %>% mutate(Age = replace_na(Age, ageMid))
+# Check for duplicates
+df[duplicated(df),]
+# rename columns
+df <- df %>% 
+  select(everything()) %>% 
+  rename(gender = Choose.your.gender, course=What.is.your.course., age = Age,
+         "year of study" = Your.current.year.of.Study, CGPA = What.is.your.CGPA.)
