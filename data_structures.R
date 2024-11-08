1f33d819c072f0b88b320583061d720b13b6b00f
Man Njoro <elijohnmwoho@gmail.com>
2024-11-08 15:21:30 +0300

    list

--- data_structures.R
+++ data_structures.R
@@ -8,3 +8,10 @@ repeat_each <- rep(c(1,2,3), each=3)
 repeat_times <- rep(c(1,2,3), times=3)
 repeat_independent <- rep(c(1,2,3), times = c(5, 2,1))
 numbers <- seq(from = 0, to = 100, by = 20)
+
+#List
+fruits <- list("apple", "banana", "oranges", "berry")
+fruits <- append(fruits,"mango")
+fruits[4] <- "strawberry"
+length(fruits)
+"grape" %in% fruits
